c476d231304d8126cf3f5b8a47864cadbd604fee
Ravali N <ravali.nk@gmail.com>
2018-01-25 17:57:52 -0500

    bootstrap changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -24,16 +24,33 @@ myApp.config(function ($routeProvider, $locationProvider) {
         $http.get("http://localhost:61268/api/Customers").then(function (response) {
             $scope.Customer = response.data;
         });
-        $scope.addcus = function (current) {
-            $scope.Customer.push(current);
+           
+        $scope.selected = {};
+        $scope.gettemplate = function (c) {
+            if (c.CustomerId === $scope.selected.CustomerId) {
+                return 'edittemp';
+            }
+            else return 'hometemp';
         };
-
+        $scope.editcus = function (c) {
+            $scope.selected = angular.copy(c);
+        };
         $scope.deletecus = function (c) {
             var index = $scope.Customer.indexOf(c);
-            alert("deleting index" + " " + index);
             $scope.Customer.splice(index, 1);
+        };
+        $scope.reset = function () {
+            $scope.selected = {};
+        };
+        $scope.savecustdetails = function (index) {
+            $scope.Customer[index] = angular.copy($scope.selected);
+            $scope.reset();
+        };
+        $scope.addcust = function (index) {
+            $scope.Customer[index] = angular.copy($scope.selected);
+            $scope.reset();
         }
-        $scope.current = {};
+
 
     })
 
